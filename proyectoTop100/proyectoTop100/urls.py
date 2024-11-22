0761836012c55ca519a67879db452b3ccc880712
jonlaasa <jon.lasa@opendeusto.es>
2024-11-22 13:55:18 +0100

    Multilinguismo

--- proyectoTop100/proyectoTop100/urls.py
+++ proyectoTop100/proyectoTop100/urls.py
@@ -1,26 +1,18 @@
-"""
-URL configuration for proyectoTop100 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns  # Importa i18n_patterns
 
-
+# Configuración de URLs
 urlpatterns = [
-    path('appTop100Django/', include('appTop100Django.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('i18n/', include('django.conf.urls.i18n')),  # Ruta para manejar los idiomas
+]
+
+# Aplica i18n_patterns para las rutas que se deben adaptar al idioma
+urlpatterns += i18n_patterns(
+    path('appTop100Django/', include('appTop100Django.urls')),  # Esto será accesible con el idioma
+)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
