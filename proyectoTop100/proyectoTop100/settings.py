6355d63b5bc718a20f27ea3457198ea9226f2e6a
aitoormerodioo <aitor.merodio@opendeusto.es>
2024-12-04 12:01:08 +0100

    admin_site

--- proyectoTop100/proyectoTop100/settings.py
+++ proyectoTop100/proyectoTop100/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'proyectoTop100.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
